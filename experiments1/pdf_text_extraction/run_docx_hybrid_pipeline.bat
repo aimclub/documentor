5e8dda039ce4b79fe36f6e7f6ba6085b416aafbc
ab211437 <120978708+ab211437@users.noreply.github.com>
2026-03-03 03:22:42 +0300

    Delete experiments1 directory

--- experiments1/pdf_text_extraction/run_docx_hybrid_pipeline.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Команда для запуска комбинированного пайплайна DOCX на одном файле
-REM Использование: run_docx_hybrid_pipeline.bat "путь_к_файлу.docx"
-
-python experiments\pdf_text_extraction\docx_hybrid_pipeline.py "E:\easy\documentor\documentor_langchain\experiments\pdf_text_extraction\test_folder\Диплом.docx"
-
-pause
